a0709b71db459944f9345352e38ca0ef90f4163d
Juan Pedro Pont Verges <pedro@cacao.to>
2022-08-02 20:49:51 -0300

    advances

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,8 +5,8 @@ import LogoFooter from 'public/logo.png'
 
 const Layout: React.FC<any> = (props) => {
     return (
-        <div className="h-screen w-screen">
-            <nav className={"flex flex-row justify-between items-center w-screen px-4 shadow-xl"}>
+        <div className="h-screen w-full">
+            <nav className={"flex flex-row justify-between items-center px-4 shadow-xl bg-[#27253B]"}>
                 <div>
                     <Image src={LogoCacao} alt='Cacao' width={200} height={140} />
                 </div>
@@ -16,10 +16,10 @@ const Layout: React.FC<any> = (props) => {
                     <li>Contacto</li>
                 </div>
             </nav>
-            <main>
+            <main className="w-full">
                 {props.children}
             </main>
-            <footer className="p-4 bg-white sm:p-6 dark:bg-gray-900 w-full bottom-0 ">
+            <footer className="p-4 bg-[#F1F1F1] sm:p-6 text-black w-full">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <a href="https://cacao.to/" className="flex items-center">
@@ -28,8 +28,8 @@ const Layout: React.FC<any> = (props) => {
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Servicios</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
+                            <h2 className="mb-6 text-sm font-semibold uppercase">Servicios</h2>
+                            <ul>
                                 <li className="mb-4">
                                     <a href="#" className="hover:underline">Encuestas</a>
                                 </li>
@@ -39,16 +39,16 @@ const Layout: React.FC<any> = (props) => {
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
+                            <h2 className="mb-6 text-sm font-semibold uppercase">Follow us</h2>
+                            <ul>
                                 <li className="mb-4">
                                     <a href="https://github.com/JuanPedroPontVerges" className="hover:underline ">Linkedin</a>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
+                            <h2 className="mb-6 text-sm font-semibold uppercase">Legal</h2>
+                            <ul>
                                 <li className="mb-4">
                                     <a href="#" className="hover:underline">Privacy Policy</a>
                                 </li>
@@ -59,12 +59,12 @@ const Layout: React.FC<any> = (props) => {
                         </div>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Cacao™</a>. Derechos reservados.
+                    <span className="text-sm sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Cacao™</a>. Derechos reservados.
                     </span>
                     <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                        <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                        <a href="#" className="hover:text-gray-900 dark:hover:text-white">
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
                         </a>
                     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
 import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 import Image from 'next/image';
 import Dasboard from 'public/dashboard.png'
+import Iphones from 'public/iphones.png'
 
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["hello", { name: 'Juan Pedro' }]);
   return (
-    <div className='flex flex-row justify-between'>
-      <div className={"flex flex-col pl-20 gap-y-20 pt-20"}>
-        <div>
-          <h1 className={"uppercase font-bold text-xl"}>Plataforma web para
-            <br />vendedores
-          </h1>
-        </div>
-        <div>
-          <h2 className='text-[#F88600] text-6xl font-extrabold'>
-            Tu cliente, <br /> con vos
-          </h2>
+    <div className='flex flex-col gap-y-14'>
+      <div className={'lg:px-60 sm:px-20  bg-[#27253B]'}>
+        {/* Banner */}
+        <div className='flex flex-row justify-between'>
+          <div className={"flex flex-col pt-20"}>
+            <div>
+              <h1 className={"uppercase font-bold text-2xl"}>Plataforma web para
+                <br />vendedores
+              </h1>
+            </div>
+            <div>
+              <h2 className='text-[#F88600] text-8xl font-extrabold pt-20'>
+                Tu cliente, <br /> con vos
+              </h2>
+            </div>
+          </div>
+          <div className='pt-20'>
+            <Image src={Dasboard} alt={'Cacao métricas'} />
+          </div>
         </div>
       </div>
-      <div className='pt-20 pr-20'>
-        <Image src={Dasboard} height={637} width={757} alt={'Cacao métricas'} />
+      <div className={'lg:px-60 sm:px-20  bg-white'}>
+        {/* Request your demo */}
+        <div className='flex flex-row justify-between'>
+          <div className='basis-1/2'>
+            <Image src={Iphones} alt={'Iphones'} />
+          </div>
+          <div className='basis-1/2 flex flex-col items-center gap-y-32'>
+            <h2 className='text-6xl font-extrabold text-right'>
+              <span className='text-[#27253B]'>
+                Todo lo que tu
+                <br />
+                comercio necesita
+              </span>
+              <br />
+              <span className='text-[#F88600] '>
+                para incrementar
+                <br />
+                ventas
+              </span>
+            </h2>
+            <div>
+              <button className={'rounded-lg p-4 px-10 text-white bg-[#F88600] font-bold'}>¡Pedí tu prueba!</button>
+          </div>
+        </div>
       </div>
     </div>
+    </div >
   )
 }
 
-export default Home
+export default Home;
