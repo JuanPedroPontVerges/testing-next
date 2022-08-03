7ee75e552a0bec1f87e74aca73665027bd6da8ae
Juan Pedro Pont Verges <pedro@cacao.to>
2022-08-03 17:00:15 -0300

    Small advances in template strucutre

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,14 @@ import type { NextPage } from 'next'
 import Image from 'next/image';
 import Dasboard from 'public/dashboard.png'
 import Iphones from 'public/iphones.png'
+import Marketing from 'public/marketing.png'
+import People from 'public/people.png'
+import GoogleReviews from 'public/google-reviews.png'
 
 const Home: NextPage = () => {
   return (
     <div className='flex flex-col gap-y-14'>
-      <div className={'lg:px-60 sm:px-20  bg-[#27253B]'}>
+      <div className={'lg:px-54 sm:px-20  bg-[#27253B]'}>
         {/* Banner */}
         <div className='flex flex-row justify-between'>
           <div className={"flex flex-col pt-20"}>
@@ -50,11 +53,35 @@ const Home: NextPage = () => {
             </h2>
             <div>
               <button className={'rounded-lg p-4 px-10 text-white bg-[#F88600] font-bold'}>¡Pedí tu prueba!</button>
+            </div>
           </div>
         </div>
       </div>
+      { /* Flag */}
+      <div className='p-2 bg-[#F88600] rounded-r-lg w-1/12'>
+        <div className={''}>Con Cacao podés</div>
+      </div>
+      <div className={'flex flex-row justify-around items-center text-black text-2xl'}>
+        <div className={'actions-container basis-1/2'}>
+          <Image src={Marketing} alt={'Campañas'} />
+          <p className='w-2/4'>
+            Realizar publicidad dirigida
+          </p>
+        </div>
+        <div className={'actions-container basis-1/2'}>
+          <Image src={People} alt={'Campañas'} />
+          <p className='w-2/4'>
+            Llevar un control del personal y de los estándares de la marca
+          </p>
+        </div>
+        <div className={'actions-container basis-1/2'}>
+          <Image src={GoogleReviews} alt={'Campañas'} />
+          <p className='w-2/4'>
+            Gestionar las críticas negativas y aumentar las positivas
+          </p>
+        </div>
+      </div>
     </div>
-    </div >
   )
 }
 
