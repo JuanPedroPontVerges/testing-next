384730594cc59e236e2ee1b1ef74c09be08ac55b
Juan Pedro Pont Verges <pedro@cacao.to>
2022-09-16 21:27:23 -0300

    Advances in carrusel

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -11,26 +11,18 @@ const Carrousel: React.FC<CarrouselProps> = ({ className }) => {
                     <h3 className={'font-regular text-4xl text-center text-[#343252]'}>Algunos de nuestros <strong className='font-black'>clientes</strong></h3>
                 </div>
             </div>
-            <div className='flex gap-4 overflow-x-auto snap-mandatory'>
+            <div className="items">
                 <div className={'p-9 text-center bg-white rounded text-black snap-start w-64'}>
-                    <h2 className='text-4xl'>Tal cual</h2>
-                    {/* <Image src={JuanPedro} alt={'testing'} layout={'fixed'} /> */}
+                    Tal cual
                 </div>
                 <div className={'p-9 text-center bg-white rounded text-black snap-start w-64'}>
-                    <h2 className='text-4xl'>Tal cual</h2>
-                    {/* <Image src={JuanPedro} alt={'testing'} layout={'fixed'} /> */}
+                    Tal cual
                 </div>
                 <div className={'p-9 text-center bg-white rounded text-black snap-start w-64'}>
-                    <h2 className='text-4xl'>Tal cual</h2>
-                    {/* <Image src={JuanPedro} alt={'testing'} layout={'fixed'} /> */}
+                    Tal cual
                 </div>
                 <div className={'p-9 text-center bg-white rounded text-black snap-start w-64'}>
-                    <h2 className='text-4xl'>Tal cual</h2>
-                    {/* <Image src={JuanPedro} alt={'testing'} layout={'fixed'} /> */}
-                </div>
-                <div className={'p-9 text-center bg-white rounded text-black snap-start w-64'}>
-                    <h2 className='text-4xl'>Tal cual</h2>
-                    {/* <Image src={JuanPedro} alt={'testing'} layout={'fixed'} /> */}
+                    Tal cual
                 </div>
             </div>
         </div>
